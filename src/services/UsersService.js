eec34e723078b2ae7264a142e0161e5f1f833bcd
fathurrahman <fathur.ashter15@gmail.com>
2024-12-21 19:59:18 +0800

    fix: hash stored user password

--- src/services/UsersService.js
+++ src/services/UsersService.js
@@ -1,6 +1,7 @@
 const { nanoid } = require("nanoid");
 const { Pool } = require("pg");
 const InvariantError = require("../exceptions/InvariantError");
+const bcrypt = require('bcrypt');
 
 class UsersService {
     constructor(){
@@ -10,9 +11,10 @@ class UsersService {
     async addUser({username, password, fullname}){
         const id = `user-${nanoid(16)}`;
 
+        const hashedPassword = bcrypt.hash(password, 10);
         const query = {
             text: 'INSERT INTO users VALUES($1, $2, $3, $4) RETURNING id',
-            values: [id, username, password, fullname]
+            values: [id, username, hashedPassword, fullname]
         };
 
         const result = await this._pool.query(query);
