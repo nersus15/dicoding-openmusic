3e5949ac15205f23571893d4abde82ec60d37dae
fathurrahman <fathur.ashter15@gmail.com>
2024-12-21 20:10:26 +0800

    update: add function for authentications

--- src/services/UsersService.js
+++ src/services/UsersService.js
@@ -2,6 +2,7 @@ const { nanoid } = require("nanoid");
 const { Pool } = require("pg");
 const InvariantError = require("../exceptions/InvariantError");
 const bcrypt = require('bcrypt');
+const AuthenticationError = require("../exceptions/AuthenticationError");
 
 class UsersService {
     constructor(){
@@ -25,6 +26,36 @@ class UsersService {
         return result[0].id;
     }
     
+    async verifyUsername(username){
+        const query = {
+            text: 'SELECT username FROM users WHERE username=$1',
+            values: [username]
+        };
+        const result = await this._pool.query(query);
+        if(result.rows.length > 0){
+            throw new InvariantError(`Username ${username} sudah terdaftar`);
+        }
+    }
+
+    async verifyUserCredential(username, password){
+        const query = {
+            text: 'SELECT id, password FROM users WHERE username=$1',
+            values: [username]
+        };
+        const result = await this._pool.query(query);
+        if(!result.rows.length){
+            throw new InvariantError(`User dengan username ${username} tidak ditemukan`);
+        }
+
+        const {id, password:hashedPassword} = result.rows[0];
+        const match = await bcrypt.compare(password, hashedPassword);
+
+        if(!match){
+            throw new AuthenticationError(`Password untuk user ${username} Salah`);
+        }
+
+        return id;
+    }
 }
 
 module.exports = UsersService;
