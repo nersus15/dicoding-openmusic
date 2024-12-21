ad23b96451de952f94210f82bfc0ceca3be93f02
fathurrahman <fathur.ashter15@gmail.com>
2024-12-21 19:55:43 +0800

    prepare for authentication & authorization

--- src/server.js
+++ src/server.js
@@ -7,10 +7,16 @@ const AlbumsValidator = require('./validator/albums');
 const albums = require('./api/albums');
 const songs = require('./api/songs');
 const SongsValidator = require('./validator/songs');
+const UsersService = require('./services/UsersService');
+const users = require('./api/users');
+const UsersValidator = require('./validator/users');
+
+const Jwt = require('@hapi/jwt');
 
 (async () => {
     const albumService = new AlbumsService();
     const songsService = new SongsService();
+    const usersService = new UsersService();
 
     const server = Hapi.server({
         port: process.env.PORT,
@@ -21,6 +27,27 @@ const SongsValidator = require('./validator/songs');
             }
         }
     });
+
+    await server.register({
+        plugin: Jwt
+    });
+
+    server.auth.strategy('jwt_auth', 'jwt', {
+        keys: process.env.ACCESS_TOKEN_KEY,
+        verify: {
+            aud: false,
+            iss: false,
+            sub: false,
+            maxAgeSec: process.env.ACCESS_TOKEN_AGE
+        },
+        validate: (artifacts) => ({
+            isValid: true,
+            credentials: {
+                userId: artifacts.decoded.payload.userId
+            }
+        })
+    });
+
     // Register Hapi Plugin Here
     await server.register([
        {
@@ -36,7 +63,14 @@ const SongsValidator = require('./validator/songs');
             service: songsService,
             validator: SongsValidator
         }
-       } 
+       },
+       {
+        plugin: users,
+        options:{
+            service: usersService,
+            validator: UsersValidator,
+        }
+       }
     ]);
 
     // Add Extension Here
